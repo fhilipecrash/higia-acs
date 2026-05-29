0d1157fd75b050e4b198d339e2059d12eef9746d
Fhilipe Coelho <fhilipecoelho.dev@gmail.com>
2026-05-29 10:11:51 -0300

    feat: add router dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,46 @@
 import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/higia-acs/',
-  plugins: [react(), VitePWA({
-    registerType: 'prompt',
-    injectRegister: 'auto',
+  // base: '/higia-acs/',
+  plugins: [
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
+    react(),
+    VitePWA({
+      registerType: 'prompt',
+      injectRegister: 'auto',
+      strategies: 'generateSW',
 
-    pwaAssets: {
-      disabled: false,
-      config: true,
-    },
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
 
-    manifest: {
-      name: 'pwa-app',
-      short_name: 'pwa-app',
-      description: 'pwa-app',
-      theme_color: '#ffffff',
-    },
+      manifest: {
+        name: 'pwa-app',
+        short_name: 'pwa-app',
+        description: 'pwa-app',
+        theme_color: '#ffffff',
+      },
 
-    workbox: {
-      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
-      cleanupOutdatedCaches: true,
-      clientsClaim: true,
-    },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
 
-    devOptions: {
-      enabled: false,
-      navigateFallback: 'index.html',
-      suppressWarnings: true,
-      type: 'module',
-    },
-  })],
+      devOptions: {
+        enabled: false,
+        navigateFallback: 'index.html',
+        suppressWarnings: true,
+        type: 'module',
+      },
+    })
+  ],
 })
