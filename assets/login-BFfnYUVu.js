059849a02a6cdd4b1d80dd364aadbff7adc9c6dd
fhilipecrash <fhilipecrash@users.noreply.github.com>
2026-05-29 17:51:25 +0000

    deploy: e9e9fcd7a3a23da26ae4954bdb95d05ae510bdb0

--- assets/login-BFfnYUVu.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-DUjWK4dq.js";function n(){return o.jsx("div",{children:'Hello "/about"!'})}export{n as component};
--- registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/higia-acs/sw.js', { scope: '/higia-acs/' })})}
