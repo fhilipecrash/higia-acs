4fe4b068195b784155b47ab92611b5a24e0cfa14
fhilipecrash <fhilipecrash@users.noreply.github.com>
2026-05-29 13:17:33 +0000

    deploy: 82f755545bff90d05d224030af1ec0cad557b9b0

--- assets/about-CCnneMAO.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-CrEA0IUu.js";function n(){return o.jsx("div",{children:'Hello "/about"!'})}export{n as component};
--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/higia-acs/sw.js', { scope: '/higia-acs/' })})}
